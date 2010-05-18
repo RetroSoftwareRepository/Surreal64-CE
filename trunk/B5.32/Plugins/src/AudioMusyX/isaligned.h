94078616bc36a95f42581ca199a5725052b9de25
weinerschnitzel <weinerschnitzel@5f6611e7-9c45-4e5e-9076-604b0fa43fd0>
2010-05-18 17:53:18 +0000

    Clean up the trunk

--- trunk/B5.32/Plugins/src/AudioMusyX/isaligned.h
+++
@@ -1,3 +0,0 @@
-//switches to control compiler
-
-//#define ASSUME_ALWAYS_ALIGNED 1
--- trunk/B5.32/Plugins/src/video6.11/_BldNum.h
+++
@@ -1,5 +0,0 @@
-//
-#define BUILDNUM_ENABLED (1)
-#define MODIFY_VERSIONINFO (1)
-#define FILE_VERSION "6.1.2"
-#define BUILD_NUMBER "612"
