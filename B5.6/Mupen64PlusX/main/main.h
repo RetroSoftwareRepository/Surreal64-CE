ff5d9b7ce04db81833f8360b69d368778330ce13
weinerschnitzel <weinerschnitzel@5f6611e7-9c45-4e5e-9076-604b0fa43fd0>
2013-10-17 00:11:20 +0000

    Changed: Turned some optimizations off in PJ64 1.6 for stability Added: initial port of Mupen64PlusX, This is a port of libretro-mupen64plus and intends to give RetroArch's N64 emulator Xbox1 support.  -plugin.c is starting to be rewritten for static linking  -debugger doesn't link ?? oh well...  -some changes were made to comply with vs2003  -Ari64's dynarec is not compatible

---
+++ B5.6/Mupen64PlusX/main/main.h
@@ -0,0 +1,54 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *   Mupen64plus - main.h                                                  *
+ *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
+ *   Copyright (C) 2012 CasualJames                                        *
+ *   Copyright (C) 2002 Blight                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "../api/m64p_types.h"
+
+/* globals */
+extern m64p_handle g_CoreConfig;
+
+extern int g_MemHasBeenBSwapped;
+extern int g_EmulatorRunning;
+
+extern m64p_frame_callback g_FrameCallback;
+
+void new_frame(void);
+
+int  main_set_core_defaults(void);
+void main_message(m64p_msg_level level, unsigned int osd_corner, const char *format, ...);
+
+m64p_error main_run(void);
+void main_stop(void);
+void main_toggle_pause(void);
+void main_advance_one(void);
+
+m64p_error main_core_state_query(m64p_core_param param, int *rval);
+m64p_error main_core_state_set(m64p_core_param param, int val);
+
+m64p_error main_read_screen(void *pixels, int bFront);
+
+m64p_error main_reset(int do_hard_reset);
+
+#endif /* __MAIN_H__ */
+
