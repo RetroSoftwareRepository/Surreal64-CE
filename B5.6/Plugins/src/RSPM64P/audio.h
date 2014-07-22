c2e33ab3fb80fd6b419aa9be0ae5e8ebea576d05
weinerschnitzel <weinerschnitzel@5f6611e7-9c45-4e5e-9076-604b0fa43fd0>
2014-07-22 13:30:16 +0000

    Changed: Updated HLE RSP to latest Git revision

--- B5.6/Plugins/src/RSPM64P/audio.h
+++ B5.6/Plugins/src/RSPM64P/audio.h
@@ -25,10 +25,13 @@
 #include <stddef.h>
 #include "stdint.h"
 
+#include "common.h"
+
 extern const int16_t RESAMPLE_LUT[64 * 4];
 
+int32_t rdot(size_t n, const int16_t *x, const int16_t *y);
 
-__inline static int16_t adpcm_predict_sample(uint8_t byte, uint8_t mask,
+static inline int16_t adpcm_predict_sample(uint8_t byte, uint8_t mask,
         unsigned lshift, unsigned rshift)
 {
     int16_t sample = (uint16_t)(byte & mask) << lshift;
